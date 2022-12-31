e2c21a304f8fac801bd886e2e7d093f52f8a34e9
sunn4room <sunn4room@163.com>
2022-12-31 19:19:58 +0800

    transfer config.yml to config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "sunn4room's blogs",
+	description: "Welcome to my blogs",
+	markdown: {
+	  lineNumbers: true
+	},
+	plugins: [
+	  "@vuepress/plugin-search",
+	  "@vuepress/plugin-nprogress"
+	]
+}
